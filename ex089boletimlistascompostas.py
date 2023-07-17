6d2d29bbc1f24bc669a6408635baeab58230d488
euleron7 <137542511+euleron7@users.noreply.github.com>
2023-07-17 18:32:40 -0400

    Add files via upload

---
+++ ex089boletimlistascompostas.py
@@ -0,0 +1,26 @@
+ficha = list()
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resposta = str(input('Quer continuar? [S/N]: '))
+    if resposta in 'Nn':
+        break
+print('=-' * 30)
+print(f'{"No.":<4}{"Nome":<10}{"MÉDIA":>8}')
+print('_' * 25)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('=-' * 35)
+    opc = int(input('Mostrar notas de qual aluno? (Digite [999] para parar o programa: '))
+    if opc == 999:
+        print('Finalizando...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas do Aluno {ficha[opc][0]} são {ficha[opc][1]}')
+print('Volte sempre.')
+
+
